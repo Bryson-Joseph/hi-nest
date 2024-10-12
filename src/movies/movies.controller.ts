113fa82717049200b19d318bf4eed14b5462545e
Bryson Joseph <jeffreyjefferson47@gmail.com>
2024-10-12 08:56:53 +0300

    DTOs and Validation

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,7 +8,9 @@ import {
   Body,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
-import { Movie } from './entities/Movie.entity';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -20,19 +22,22 @@ export class MoviesController {
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
     return this.moviesService.getOne(movieId);
   }
+
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
+
   @Delete(':id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
+
   @Patch(':id')
-  patch(@Param('id') movieId: string, @Body() updatedData) {
-    return { updatedMovie: movieId, ...updatedData };
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
   }
 }
